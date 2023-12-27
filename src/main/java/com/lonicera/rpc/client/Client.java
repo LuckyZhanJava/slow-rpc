46370470afb14927b1d81bcb4922019c2ca99633
bowei.li <bowei.li@i-tudou.com>
2023-12-27 15:18:27 +0800

    'fix:channel-pool-timeout'

--- src/main/java/com/lonicera/rpc/client/Client.java
+++ src/main/java/com/lonicera/rpc/client/Client.java
@@ -11,18 +11,19 @@ import io.netty.channel.ChannelFutureListener;
 import io.netty.channel.ChannelPipeline;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.pool.AbstractChannelPoolHandler;
+import io.netty.channel.pool.ChannelHealthChecker;
 import io.netty.channel.pool.ChannelPool;
 import io.netty.channel.pool.FixedChannelPool;
+import io.netty.channel.pool.FixedChannelPool.AcquireTimeoutAction;
 import io.netty.channel.socket.nio.NioSocketChannel;
 import io.netty.handler.ssl.SslContext;
 import io.netty.handler.ssl.SslContextBuilder;
+import io.netty.util.NettyRuntime;
 import io.netty.util.concurrent.DefaultThreadFactory;
+import io.netty.util.concurrent.Future;
+import io.netty.util.concurrent.GenericFutureListener;
 import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.Future;
 import java.util.concurrent.ThreadFactory;
-import java.util.concurrent.TimeUnit;
-import java.util.concurrent.TimeoutException;
 
 public class Client {
 
@@ -44,7 +45,8 @@ public class Client {
     }
   };
 
-  public Client(Protocol<?, ?> protocol, boolean useSSL, String host, int port, ClientOptions clientOptions) {
+  public Client(Protocol<?, ?> protocol, boolean useSSL, String host, int port,
+      ClientOptions clientOptions) {
     bootstrap = new Bootstrap()
         .channel(NioSocketChannel.class)
         .group(new NioEventLoopGroup(CLIENT_THREAD_FACTORY))
@@ -52,46 +54,62 @@ public class Client {
     this.host = host;
     this.port = port;
     this.clientOptions = clientOptions;
-    channelPool = new FixedChannelPool(bootstrap, new AbstractChannelPoolHandler() {
+    channelPool = new FixedChannelPool(
+        bootstrap,
+        new AbstractChannelPoolHandler() {
 
-      @Override
-      public void channelCreated(Channel ch) throws Exception {
-        ChannelPipeline pipeline = ch.pipeline();
-        if(useSSL){
-          SslContext sslCtx = SslContextBuilder.forClient().build();
-          pipeline.addLast("ssl", sslCtx.newHandler(ch.alloc()));
-        }
-        pipeline
-            .addLast(protocol.decodeResponseHandlers())
-            .addLast(protocol.encodeRequestHandlers())
-            .addLast(new ChannelRpcRequestHandler())
-        ;
-      }
-    }, 10);
+          @Override
+          public void channelCreated(Channel ch) throws Exception {
+            ChannelPipeline pipeline = ch.pipeline();
+            if (useSSL) {
+              SslContext sslCtx = SslContextBuilder.forClient().build();
+              pipeline.addLast("ssl", sslCtx.newHandler(ch.alloc()));
+            }
+            pipeline
+                .addLast(protocol.decodeResponseHandlers())
+                .addLast(protocol.encodeRequestHandlers())
+                .addLast(new ChannelRpcRequestHandler())
+            ;
+          }
+        },
+        ChannelHealthChecker.ACTIVE,
+        AcquireTimeoutAction.FAIL,
+        clientOptions.getPoolOptions().getAcquireTimeoutMillis(),
+        NettyRuntime.availableProcessors() * 2,
+        Integer.MAX_VALUE
+    );
   }
 
 
   public CompletableFuture<RpcResponse> write(RpcRequest request, RequestOptions options) {
     Future<Channel> channelFuture = channelPool.acquire();
 
-    Channel channel = null;
-    try {
-      channel = channelFuture.get(clientOptions.getPoolOptions().getAcquireTimeoutMillis(), TimeUnit.MILLISECONDS);
-    } catch (InterruptedException | ExecutionException | TimeoutException e) {
-      throw new RpcException(RpcException.CONNECT_ERROR, e);
-    }
     ChannelRpcRequest rpcRequest = new ChannelRpcRequest(request, options.getWriteTimeoutMillis(),
         options.getReadTimeoutMillis());
     CompletableFuture<RpcResponse> respFuture = rpcRequest.getResponseFuture();
-    channel.writeAndFlush(rpcRequest).addListener(new ChannelFutureListener() {
+
+    channelFuture.addListener(new GenericFutureListener<Future<Channel>>() {
+
       @Override
-      public void operationComplete(ChannelFuture future) throws Exception {
-        channelPool.release(future.channel());
-        if(future.cause() != null){
-          respFuture.completeExceptionally(new RpcException(RpcException.CLIENT_ERROR, future.cause()));
+      public void operationComplete(Future<Channel> future) throws Exception {
+        if (future.cause() != null) {
+          respFuture.completeExceptionally(future.cause());
+        } else {
+          future.get().writeAndFlush(rpcRequest).addListener(new ChannelFutureListener() {
+            @Override
+            public void operationComplete(ChannelFuture future) throws Exception {
+              channelPool.release(future.channel());
+              if (future.cause() != null) {
+                respFuture
+                    .completeExceptionally(
+                        new RpcException(RpcException.CLIENT_ERROR, future.cause()));
+              }
+            }
+          });
         }
       }
     });
+
     return respFuture;
   }
 }
